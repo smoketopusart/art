05620ee1cfa9b9e4e019db3bbfd70d770aabcaf1
radrury <167112571+radrury@users.noreply.github.com>
2026-09-01 10:45:59 -0400

    testing rss stuff

---
+++ debug/updateRSS.js
@@ -0,0 +1,24 @@
+//to do:
+// link to our RSS feed
+// read RSS feed, see where things should be appended
+// onClick for the "post", we will start writing at the point mentioned
+
+function OnLoad(){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+    if (xhttp.readyState == 4 && xhttp.status == 200) {
+
+        var xmlDoc = xhttp.responseXML; //important to use responseXML here
+    }
+    xhttp.open("GET", "/rss.xml", true);
+    xhttp.send();
+}
+
+// title is user defined
+// category is user defined
+// description is the MD the user created
+// post time is based on the time when the button is clicked
+// guid can be that post timestamp
+
+// should start by filling the textbox with the RSS feed, and THEN read from that text box
+// this way we can append and add multiple entries at a time
