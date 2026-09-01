1b1dd170f2e2bb85bd82f4d9105348b15224228c
radrury <167112571+radrury@users.noreply.github.com>
2026-09-01 14:29:35 -0400

    more testing

--- debug/updateRSS.js
+++ debug/updateRSS.js
@@ -2,34 +2,80 @@
 // link to our RSS feed
 // read RSS feed, see where things should be appended
 // onClick for the "post", we will start writing at the point mentioned
+var xmlDoc;
+
+console.log("loading");
+    fetch("https://smoketopus.art/rss.xml")
+            .then((response) => response.text())
+            .then((xmlString) => {
+                const parser = new DOMParser();
+                xmlDoc = parser.
+                    parseFromString(xmlString, "text/xml");
+            
+                //
+
+                //document.getElementById("rssFeed").innerHTML = xmlString;
+
+
+                
+            })
 
 function loadRSS(){
-    console.log("loading");
-    var xhttp = new XMLHttpRequest();
-    xhttp.onloadstart = function() {
-        if (xhttp.readyState == 4 && xhttp.status == 200) {
-            console.log("acceptable");
-
-            console.log("xmldoc is " + xmlDoc);
-            var xmlDoc = xhttp.responseXML; //important to use responseXML here
-            var serializer = new XMLSerializer();
-            //var xmlString = serializer.serializeToString(xmlDoc);
-            document.getElementById("rssFeed").value += xmlDoc;        
-        }else{
-            console.log("Hitting errors" + xhttp.statusText);
-            document.getElementById("rssFeed").innerHTML = "test";
-
-        }
-        xhttp.open("GET", "https://smoketopus.art/rss.xml", true);
-        xhttp.send();
-
-    }
+    
 }
 // title is user defined
 // category is user defined
 // description is the MD the user created
 // post time is based on the time when the button is clicked
 // guid can be that post timestamp
+function PostNewEntry(){
+    //var items = xmlDoc.getElementsByTagName("item");
+
+    let newItem = xmlDoc.createElement("item");
+    let newTitle = xmlDoc.createElement("title");
+    newTitle.innerHTML = "testing new shit";
+    let newCategory = xmlDoc.createElement("category");
+    newCategory.innerHTML = "test";
+    let newLink = xmlDoc.createElement("link");
+    newLink.innerHTML = "https://smoketopus.art";
+    let newImage = xmlDoc.createElement("image");
+    newImage.innerHTML = "https://smoketopus.art/banner.png";
+    let newDescription = xmlDoc.createElement("description");
+    newDescription.innerHTML = "testing my setup to make sure it all works! \n";
+    newDescription.innerHTML += 
+    "<![CDATA[<img src=\"" + newImage + "\"/>]]>"
+    let pubDate = xmlDoc.createElement("pubDate");
+    const dateHeader = new Date().toLocaleString('en-GB', {
+        timeZone: 'UTC',
+        hour12: false,
+        weekday: 'short',
+        year: 'numeric',
+        month: 'short',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        }).replace(/(?:(\d),)/, '$1') + ' GMT';
+    pubDate.innerHTML = dateHeader;
+    let guID = xmlDoc.createElement("guid");
+    guID.innerHTML = dateHeader;
+
+
+    newItem.appendChild(newTitle);
+    newItem.appendChild(newCategory);
+    newItem.appendChild(newLink);
+    newItem.appendChild(newDescription);
+    newItem.appendChild(pubDate);
+    newItem.appendChild(guID);
+
+
+
+    xmlDoc.getElementsByTagName("item")[xmlDoc.getElementsByTagName("item").length-1].appendChild(newItem);
+
+    var xmlNewText = new XMLSerializer().serializeToString(xmlDoc);
+    document.getElementById("rssFeed").innerHTML = xmlNewText;
+
+}
 
 // should start by filling the textbox with the RSS feed, and THEN read from that text box
 // this way we can append and add multiple entries at a time
